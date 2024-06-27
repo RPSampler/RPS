d7dcdf8f0e43e9eae5239d073af5fdc079ce39a1
RPSampler <icdm24code@gmail.com>
2024-06-28 00:13:35 +0200

    Update ResPat.py

--- ResPat.py
+++ ResPat.py
@@ -1,83 +1,125 @@
-
 import time
-from random import random
-from decimal import Decimal
+from mpmath import mp
 
-def ResPat_ExpJ_Damping(dataset, sample_size, tmps, alpha, batchsize):
+def ResPat_ExpJ_Damping(dataset, sample_size, tmps, alpha, batchsize, df, idf):
     reservoir = []
     jump = 0
     timestamp = 0
-    nbInstInBatch = 0
     out_of_timing = False
+    nbInstInBatch = 0
+    factor = alpha
+    
     with open(dataset, 'r') as base:
         line = base.readline()
         timestamp += 1
+        
         while line:
             nbInstInBatch += 1
             instance = line.split()
             size = len(instance)
             i = 0
-            w = 2**size
-            while i+jump < w:
+            w = 2 ** size
+            
+            while i + jump < w:
                 i += jump
                 pattern = []
-                binv =format(i, 'b')
-                for l in range(len(binv)):
-                    if int(binv[l])==1:
-                        pattern.append(instance[size-len(binv):][l])
-                j, mk = minkey(reservoir, timestamp, alpha, sample_size)
-                pat_key = mk + Decimal(random())*(1-mk)
-                reservoir = updateSample(reservoir, sample_size, pattern, j, pat_key, timestamp)
-                x =int( Decimal(random()).ln()/(mk.ln()) )
+                binv = format(int(i), 'b').zfill(size)
+                
+                # Create pattern based on binary representation
+                for l in range(size):
+                    if binv[l] == '1':
+                        pattern.append(instance[l])
+                
+                # Find minimum key in reservoir
+                j, mk = minkey(reservoir, timestamp, df, sample_size)
+                
+                # Generate pattern key
+                pat_key = mk + mp.rand() * (1 - mk)
+                
+                # Update reservoir with the pattern
+                reservoir = updateSample(reservoir, sample_size, pattern, j, pat_key, timestamp, idf)
+                
+                # Determine jump size dynamically
+                _, mk = minkey(reservoir, timestamp, df, sample_size)
+                x = 0
+                if mk > 0:
+                    x = mp.mpf(int(mp.log(mp.rand()) / mp.log(mk)))
                 jump = x + 1
-                if time.time()-tmps > 3600:
+                
+                # Check if time limit exceeded
+                if time.time() - tmps > 3600:
                     print("Out of timing")
                     out_of_timing = True
                     break
+            
             if out_of_timing:
                 break
-            jump = (i+jump) - w
+            
+            jump = (i + jump) - w
             line = base.readline()
+            
+            # Check batch size and update factors
             if nbInstInBatch == batchsize:
                 timestamp += 1
+                if factor > 0:
+                    df *= factor
+                    idf *= 1 / factor
                 nbInstInBatch = 0
+    
     return reservoir
 
 
-def updateSample(reservoir, sample_size, pattern, j, mk, timestamp):
-    c = Decimal(alpha)*Decimal(timestamp)
-    r = mk**Decimal(1/c.exp())
-    if len(reservoir)<sample_size:
+def updateSample(reservoir, sample_size, pattern, j, pat_key, timestamp, idf):
+    r = mp.mpf(pat_key ** idf)
+    
+    if len(reservoir) < sample_size:
         reservoir.append((pattern, r, timestamp))
     else:
         reservoir[j] = (pattern, r, timestamp)
+    
     return reservoir
 
-def minkey(reservoir, timestamp, alpha, sample_size):
+
+def minkey(reservoir, timestamp, df, sample_size):
     if len(reservoir) < sample_size:
-        return 0, Decimal(0.)
-    j, mk = 0, Decimal(0.9999999999)
-    c = Decimal(alpha)*Decimal(timestamp)
+        return 0, mp.mpf(0.)
+    
+    j, mk = 0, mp.mpf(0.9999999999999999)
+    
     for i in range(len(reservoir)):
-        if Decimal(reservoir[i][1])**c.exp() < mk:
-            mk = Decimal(reservoir[i][1])**c.exp()
+        if reservoir[i][1] < mk:
+            mk = reservoir[i][1]
             j = i
+            if mk == 0:
+                break
+    
+    mk = mk ** df
+    
+    if mk == 1:
+        mk = 0.9999999999999999
+    
     return j, mk
 
+
 if __name__ == '__main__':
-    databases = ["ORetail", "kddcup99", "POWERC", "SUSY"]
+    databases = ["POWERC", "ORetail", "kddcup99", "SUSY"]
     sample_size = 10000
-    alpha = 0.1 # damping factor
+    alpha_values = [0, 0.1, 0.5]
     batchsize = 1000
+    
+    df = mp.mpf(1)
+    idf = mp.mpf(1)
+    
     for dataname in databases:
-        streamdata = "Benchmark/Itemset/"+dataname+".num"
-        print(dataname)
-        print("sample_size",sample_size) 
+        for alpha in alpha_values:
+            streamdata = "Benchmark/Itemset/" + dataname + ".num"
+            print(dataname)
+            print("sample_size", sample_size) 
+    
+            tmps = time.time()
+            reservoir = ResPat_ExpJ_Damping(streamdata, sample_size, tmps, mp.mpf(alpha), batchsize, df, idf)
+            tmps = time.time() - tmps
+    
+            print(f"===  Execution time {dataname} with damping factor {alpha}: {tmps}")
 
-        tmps = time.time()
-        reservoir = ResPat_ExpJ_Damping(streamdata, sample_size, tmps, Decimal(alpha), batchsize)
-        tmps = time.time()-tmps
 
-        print(f"===  Execution time {dataname} with damping factor {alpha}: {tmps}")
-
-        
